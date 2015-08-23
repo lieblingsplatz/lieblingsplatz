6e7a451917ed8de63c690ccff8251f100aab13fe
fuzzylogic2000 <katharina.albers@gmail.com>
2015-08-23 21:38:29 +0200

    test view returns html

--- lieblingsplatz_website/map/tests.py
+++ lieblingsplatz_website/map/tests.py
@@ -1,6 +1,8 @@
 # coding=utf-8
 from django.core.urlresolvers import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+from django.template.loader import render_to_string
 from map.models import Kita
 from map.views import home_page
 
@@ -44,6 +46,12 @@ class HomePageTest(TestCase):
     def test_root_urlresolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_html)
 
 
 class MapFunctionNotOverwritten(TestCase):
