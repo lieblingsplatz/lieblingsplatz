c33583c56489b13bbdc02d38b5db8526fc8e0f5a
fuzzylogic2000 <katharina.albers@gmail.com>
2015-08-23 20:13:46 +0200

    registered admin panel

--- lieblingsplatz_website/map/admin.py
+++ lieblingsplatz_website/map/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from map.models import Kita
+
+class KitaAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+admin.site.register(Kita, KitaAdmin)
